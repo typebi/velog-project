e3dd25af7075370793662c61902a1c2f3a25075e
typebi <typebi.dev@gmail.com>
2025-03-19 23:15:33 +0900

    feat : Add ResponseEntity and ApiResponse

--- src/main/kotlin/com/typebi/spring/api/rest/PostController.kt
+++ src/main/kotlin/com/typebi/spring/api/rest/PostController.kt
@@ -4,7 +4,10 @@ import com.typebi.spring.api.requests.PostCreateDTO
 import com.typebi.spring.api.requests.PostUpdateDTO
 import com.typebi.spring.api.responses.PostResponseDTO
 import com.typebi.spring.api.service.PostService
+import com.typebi.spring.common.response.ApiResponse
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.net.URI
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -12,29 +15,40 @@ class PostController(
     private val postService: PostService
 ) {
     @PostMapping
-    fun createPost(@RequestBody postCreateDTO: PostCreateDTO): PostResponseDTO {
-        return postService.createPost(postCreateDTO)
+    fun createPost(@RequestBody postCreateDTO: PostCreateDTO): ResponseEntity<ApiResponse<PostResponseDTO>> {
+        val postDTO = postService.createPost(postCreateDTO)
+        val location = URI.create("/api/v1/posts/${postDTO.id}")
+        val response = ApiResponse(true, postDTO, "Post created successfully")
+        return ResponseEntity.created(location).body(response)
     }
+
     @GetMapping
-    fun getPosts(): List<PostResponseDTO> {
-        return postService.getPosts()
+    fun getPosts(): ResponseEntity<ApiResponse<List<PostResponseDTO>>> {
+        val postDTOs = postService.getPosts()
+        val response = ApiResponse(true, postDTOs, "Posts retrieved successfully")
+        return ResponseEntity.ok(response)
     }
 
     @GetMapping("/{postId:\\d+}")
-    fun getPostById(@PathVariable(name = "postId") postId: Long): PostResponseDTO {
-        return postService.getPostById(postId)
+    fun getPostById(@PathVariable(name = "postId") postId: Long): ResponseEntity<ApiResponse<PostResponseDTO>> {
+        val postDTO = postService.getPostById(postId)
+        val response = ApiResponse(true, postDTO, "Post with ID $postId retrieved successfully")
+        return ResponseEntity.ok(response)
     }
 
     @PatchMapping("/{postId:\\d+}")
     fun updatePostById(
         @PathVariable(name = "postId") postId: Long,
         @RequestBody postUpdateDTO: PostUpdateDTO
-    ): PostResponseDTO {
-        return postService.updatePostById(postId, postUpdateDTO)
+    ): ResponseEntity<ApiResponse<PostResponseDTO>> {
+        val postDTO = postService.updatePostById(postId, postUpdateDTO)
+        val response = ApiResponse(true, postDTO, "User with ID $postId updated successfully")
+        return ResponseEntity.ok(response)
     }
 
     @DeleteMapping("/{postId:\\d+}")
-    fun deletePostById(@PathVariable(name = "postId") postId: Long) {
+    fun deletePostById(@PathVariable(name = "postId") postId: Long): ResponseEntity<Void> {
         postService.deletePostById(postId)
+        return ResponseEntity.noContent().build()
     }
 }
--- src/main/kotlin/com/typebi/spring/api/rest/UserController.kt
+++ src/main/kotlin/com/typebi/spring/api/rest/UserController.kt
@@ -4,7 +4,10 @@ import com.typebi.spring.api.requests.UserCreateDTO
 import com.typebi.spring.api.requests.UserUpdateDTO
 import com.typebi.spring.api.responses.UserResponseDTO
 import com.typebi.spring.api.service.UserService
+import com.typebi.spring.common.response.ApiResponse
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.net.URI
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -12,29 +15,40 @@ class UserController(
     private val userService: UserService
 ) {
     @PostMapping
-    fun createUser(@RequestBody userCreateDTO: UserCreateDTO): UserResponseDTO {
-        return userService.createUser(userCreateDTO)
+    fun createUser(@RequestBody userCreateDTO: UserCreateDTO): ResponseEntity<ApiResponse<UserResponseDTO>> {
+        val userDTO = userService.createUser(userCreateDTO)
+        val location = URI.create("/api/v1/users/${userDTO.id}")
+        val response = ApiResponse(true, userDTO, "User created successfully")
+        return ResponseEntity.created(location).body(response)
     }
+
     @GetMapping
-    fun getUsers(): List<UserResponseDTO> {
-        return userService.getUsers()
+    fun getUsers(): ResponseEntity<ApiResponse<List<UserResponseDTO>>> {
+        val userDTOs = userService.getUsers()
+        val response = ApiResponse(true, userDTOs, "Users retrieved successfully")
+        return ResponseEntity.ok(response)
     }
 
     @GetMapping("/{userId:\\d+}")
-    fun getUserById(@PathVariable(name = "userId") userId: Long): UserResponseDTO {
-        return userService.getUserById(userId)
+    fun getUserById(@PathVariable(name = "userId") userId: Long): ResponseEntity<ApiResponse<UserResponseDTO>> {
+        val userDTO = userService.getUserById(userId)
+        val response = ApiResponse(true, userDTO, "User with ID $userId retrieved successfully")
+        return ResponseEntity.ok(response)
     }
 
     @PatchMapping("/{userId:\\d+}")
     fun updateUserById(
         @PathVariable(name = "userId") userId: Long,
         @RequestBody userUpdateDTO: UserUpdateDTO
-    ): UserResponseDTO {
-        return userService.updateUserById(userId, userUpdateDTO)
+    ): ResponseEntity<ApiResponse<UserResponseDTO>> {
+        val userDTO = userService.updateUserById(userId, userUpdateDTO)
+        val response = ApiResponse(true, userDTO, "User with ID $userId updated successfully")
+        return ResponseEntity.ok(response)
     }
 
     @DeleteMapping("/{userId:\\d+}")
-    fun deleteUserById(@PathVariable(name = "userId") userId: Long) {
+    fun deleteUserById(@PathVariable(name = "userId") userId: Long): ResponseEntity<Void> {
         userService.deleteUserById(userId)
+        return ResponseEntity.noContent().build()
     }
 }
