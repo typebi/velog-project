3f0a80b5d248980af658a77a274b62dd7c92eef1
typebi <typebi.dev@gmail.com>
2025-03-20 01:36:50 +0900

    feat : Add ApiResponse in DELETE API and status code in Apiresponse

--- src/main/kotlin/com/typebi/spring/api/rest/PostController.kt
+++ src/main/kotlin/com/typebi/spring/api/rest/PostController.kt
@@ -5,6 +5,7 @@ import com.typebi.spring.api.requests.PostUpdateDTO
 import com.typebi.spring.api.responses.PostResponseDTO
 import com.typebi.spring.api.service.PostService
 import com.typebi.spring.common.response.ApiResponse
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.net.URI
@@ -18,21 +19,21 @@ class PostController(
     fun createPost(@RequestBody postCreateDTO: PostCreateDTO): ResponseEntity<ApiResponse<PostResponseDTO>> {
         val postDTO = postService.createPost(postCreateDTO)
         val location = URI.create("/api/v1/posts/${postDTO.id}")
-        val response = ApiResponse(true, postDTO, "Post created successfully")
+        val response = ApiResponse(true, postDTO, "Post created successfully", HttpStatus.CREATED.value())
         return ResponseEntity.created(location).body(response)
     }
 
     @GetMapping
     fun getPosts(): ResponseEntity<ApiResponse<List<PostResponseDTO>>> {
         val postDTOs = postService.getPosts()
-        val response = ApiResponse(true, postDTOs, "Posts retrieved successfully")
+        val response = ApiResponse(true, postDTOs, "Posts retrieved successfully", HttpStatus.OK.value())
         return ResponseEntity.ok(response)
     }
 
     @GetMapping("/{postId:\\d+}")
     fun getPostById(@PathVariable(name = "postId") postId: Long): ResponseEntity<ApiResponse<PostResponseDTO>> {
         val postDTO = postService.getPostById(postId)
-        val response = ApiResponse(true, postDTO, "Post with ID $postId retrieved successfully")
+        val response = ApiResponse(true, postDTO, "Post with ID $postId retrieved successfully", HttpStatus.OK.value())
         return ResponseEntity.ok(response)
     }
 
@@ -42,13 +43,14 @@ class PostController(
         @RequestBody postUpdateDTO: PostUpdateDTO
     ): ResponseEntity<ApiResponse<PostResponseDTO>> {
         val postDTO = postService.updatePostById(postId, postUpdateDTO)
-        val response = ApiResponse(true, postDTO, "User with ID $postId updated successfully")
+        val response = ApiResponse(true, postDTO, "User with ID $postId updated successfully", HttpStatus.OK.value())
         return ResponseEntity.ok(response)
     }
 
     @DeleteMapping("/{postId:\\d+}")
-    fun deletePostById(@PathVariable(name = "postId") postId: Long): ResponseEntity<Void> {
-        postService.deletePostById(postId)
-        return ResponseEntity.noContent().build()
+    fun deletePostById(@PathVariable(name = "postId") postId: Long): ResponseEntity<ApiResponse<Unit>> {
+        val result = postService.deletePostById(postId)
+        val response = ApiResponse(result, Unit, "Post with ID $postId deleted successfully", HttpStatus.NO_CONTENT.value())
+        return ResponseEntity.ok(response)
     }
 }
--- src/main/kotlin/com/typebi/spring/common/response/ApiResponse.kt
+++ src/main/kotlin/com/typebi/spring/common/response/ApiResponse.kt
@@ -4,4 +4,5 @@ data class ApiResponse<T> (
     val success: Boolean,
     val data: T? = null,
     val message: String? = null,
+    val code: Int
 )
