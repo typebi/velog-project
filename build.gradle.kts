44eb5ec3ef7f7deac9466c233172e63684f62e6a
typebi <typebi.dev@gmail.com>
2025-03-21 19:43:30 +0900

    feat: Add HATEOAS dependency and reorder dependency code

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,15 +20,23 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	// WEB
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.boot:spring-boot-starter-hateoas")
+
+	// DB
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	runtimeOnly("com.h2database:h2")
+
+	// TEST
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
+	// SECURITY
 	implementation("org.springframework.security:spring-security-crypto")
 }
 
